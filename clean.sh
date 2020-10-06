5000fe37aa58f602d1c833f3c79e089ac579df95
twiddler <13364827+twiddler@users.noreply.github.com>
2020-10-06 21:11:45 +0200

    Update doc; use variables and arguments; put out end result

--- clean.sh
+++ clean.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-rm plain.*
-rm password*
+rm *.tar*
--- split.sh
+++ split.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+PARTS=3
+THRESHOLD=2
+INPUT=$1
+
 # Archive the plain files
-tar -c plain > plain.tar
+tar -c $INPUT > $INPUT.tar
 
 # Split the archive into m parts such that n of them together can recover the whole archive
-gfsplit -n 2 -m 3 plain.tar
+gfsplit -n $THRESHOLD -m $PARTS $INPUT.tar
 
 # Protect the parts against data rot
-for filename in ./plain.tar.*
+for filename in ./$INPUT.tar.*
 do
     par2create -r100 -n1 -q "${filename}"
 done
+
+echo ""
+echo "Split $INPUT into $PARTS parts. You will need $THRESHOLD parts for recovering your files."
+echo -e "\e[33mTAKE CARE: Remember to remove your plain copy of the files if you do not want to keep them unprotected.\e[0m"
