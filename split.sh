b09d423d7f7e594827fc6bad604cd57b59ffbf49
twiddler <13364827+twiddler@users.noreply.github.com>
2020-10-06 21:32:58 +0200

    Pass output dir

--- split.sh
+++ split.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-INPUT=$1
-THRESHOLD=$2
-PARTS=$3
+IN=$1 # must be a directory
+OUT=$2 # must be a directory (need not exist)
+THRESHOLD=$3 # must be >= 2 and <= $PARTS
+PARTS=$4
 
-# Archive the plain files
-tar -c $INPUT > $INPUT.tar
+# Archive the input
+mkdir $OUT
+tar -c $IN > $OUT/$IN.tar
 
 # Split the archive into m parts such that n of them together can recover the whole archive
-gfsplit -n $THRESHOLD -m $PARTS $INPUT.tar
+gfsplit -n $THRESHOLD -m $PARTS $OUT/$IN.tar
+rm $OUT/$IN.tar
 
 # Protect the parts against data rot
-for filename in ./$INPUT.tar.*
+for filename in $OUT/$IN.tar.*
 do
     par2create -r100 -n1 -q "${filename}"
 done
 
 echo ""
-echo "Split $INPUT into $PARTS parts. You will need $THRESHOLD parts for recovering your files."
+echo "Split $IN into $PARTS parts. You will need $THRESHOLD parts for recovering your files."
 echo -e "\e[33mTAKE CARE: Remember to remove your plain copy of the files if you do not want to keep them unprotected.\e[0m"
